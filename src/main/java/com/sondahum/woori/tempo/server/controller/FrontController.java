cbb387c0527e49307b68d6d9c6fc94324b1b2cac
sonjongyoung <sondahum@gmail.com>
2020-08-05 14:34:40 +0900

    move create response method from streams to front controller

--- src/main/java/com/sondahum/woori/tempo/server/controller/FrontController.java
+++ src/main/java/com/sondahum/woori/tempo/server/controller/FrontController.java
@@ -3,21 +3,28 @@ package com.sondahum.woori.tempo.server.controller;
 import com.sondahum.woori.tempo.chat.WooriChatController;
 import com.sondahum.woori.tempo.metronome.MetronomeController;
 import com.sondahum.woori.tempo.server.WooriConfig;
-import com.sondahum.woori.tempo.server.packet.WooriHttpHeader;
+import com.sondahum.woori.tempo.server.packet.WooriHttpRequest;
+import com.sondahum.woori.tempo.server.packet.WooriHttpResponse;
 import com.sondahum.woori.tempo.signal.TeamSignalController;
+
+import java.io.*;
+import java.net.Socket;
 
 public class FrontController {
     private WooriController controller;
+    private WooriConfig configure;
 
     public FrontController(WooriConfig configure) {
-
+        this.configure = configure;
     }
 
-    public byte[] controllerMapping(WooriHttpHeader request) {
+    public WooriHttpResponse controllerMapping(WooriHttpRequest request) throws IOException {
         // todo request 베이스 parse해서 알맞은 controller에 전달.
-        String baseURL = request.getUrl().split("/")[0];
+        String baseURL = request.getHeader().getUrl().split("/")[0];
+        byte[] result;
+        if (baseURL.equals("/")) {
 
-        if (baseURL.equals("tempo")) {
+        } else if (baseURL.equals("tempo")) {
             controller = new MetronomeController();
         } else if (baseURL.equals("chat")) {
             controller = new WooriChatController();
@@ -25,6 +32,28 @@ public class FrontController {
             controller = new TeamSignalController();
         } else            throw new IllegalArgumentException();
 
-        return controller.requestMapping(request.getUrl());
+        result = controller.handleRequest(request.getHeader().getUrl());
+        return createResponse(request, result);
+    }
+
+    public WooriHttpResponse createResponse(WooriHttpRequest request, byte[] result) throws IOException {
+        WooriHttpResponse response = new WooriHttpResponse();
+        StringBuilder sb = new StringBuilder();
+
+        if (new File(request.getHeader().getUrl()).isFile()) {
+            sb.append("HTTP/1.1 200 OK\n");
+        } else{
+            sb.append("HTTP/1.1 404 OK\n");
+        }
+        sb.append("Content-Type: ")
+                .append(request.getHeader().getContentType())
+                .append("Content-Length: ")
+                .append(request.getHeader().getContentLength())
+                .append("\n");
+
+        response.setHeader(sb.toString());
+        response.setContent(result);
+
+        return response;
     }
 }
